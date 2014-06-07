01c802adf695e056152e5fc4e959a19115d799ae
齐宇 <11301127@bjtu.edu.cn>
2014-06-07 20:23:26 +0800

    modify

--- WebBackEnd/Teacher/TeacherAdd.aspx.cs
+++ WebBackEnd/Teacher/TeacherAdd.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,8 +8,29 @@ using System.Web.UI.WebControls;
 
 public partial class Teacher_TeacherAdd : System.Web.UI.Page
 {
+    private static readonly IBLL.ITeacher bllTeacher = BLLFactory.DataAccess.CreateTeacher();
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+
+    protected void Submit_Click(object sender, EventArgs e)
+    {
+        TeacherInfo teacher = new TeacherInfo();
+
+        teacher.Tea_Name = TeacherName.Text.ToString();
+        teacher.Tea_Nickname = TeacherNickName.Text.ToString();
+        //teacher.Tea_Image = TeacherImage.r
+        //teacher.Tea_Sex = 
+        teacher.Tea_Age = Int32.Parse(TeacherAge.SelectedValue);
+        teacher.Tea_Nation = TeacherNation.Text.ToString();
+        teacher.Tea_Skill = TeacherSkill.Text.ToString();
+        teacher.Tea_Suitable = TeacherSuitable.SelectedValue.ToString();
+        teacher.Tea_About = TeacherAbout.Text.ToString();
+        teacher.Tea_Describe = TeacherDescribe.Text.ToString();
+
+        bllTeacher.Add(teacher);
     }
 }
